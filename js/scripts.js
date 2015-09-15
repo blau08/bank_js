9bd039255d2dbc12ad235677cada1313dc02697b
Brian Lau & Justin Scott <student@epicodus.com>
2015-09-15 14:05:00 -0700

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,49 @@ function Account(accountName, accountBalance) {
   this.accountName = accountName;
   this.accountBalance = accountBalance;
 };
+
+Account.prototype.deposit = function(amount) {
+  return this.accountBalance += amount;
+};
+
+Account.prototype.withdraw = function(amount) {
+  return this.accountBalance -= amount;
+};
+
+$(document).ready(function() {
+  $("form#new-account").submit(function(event) {
+
+    var userName = $("input#account-name").val();
+    var initialDeposit = parseInt($("input#initial-deposit").val());
+
+    var newAccount = new Account(userName, initialDeposit);
+
+    $(".account-name").text(userName);
+    $(".account-balance").text(initialDeposit);
+
+    $("#info").show();
+    event.preventDefault();
+
+    $("input#account-name").val("");
+    $("input#initial-deposit").val("");
+
+    $("form#deposit-withdraw").submit(function(event) {
+
+      var newDeposit = parseInt($("input#depositAmount").val());
+      var newWithdraw = parseInt($("input#withdrawAmount").val());
+
+      if (newDeposit > 0) {
+        newBalance = newAccount.deposit(newDeposit);
+      } else if (newWithdraw > 0) {
+        newBalance = newAccount.withdraw(newWithdraw);
+      }
+
+      $(".account-name").text(newAccount.accountName);
+      $(".account-balance").text(newBalance);
+      event.preventDefault();
+
+      $("input#depositAmount").val("");
+      $("input#withdrawAmount").val("");
+    });
+  });
+});
--- specs/specs.js
+++ specs/specs.js
@@ -4,4 +4,14 @@ describe("Account", function() {
     expect(testAccount.accountName).to.equal("Jon Snow");
     expect(testAccount.accountBalance).to.equal(2000);
   });
+
+  it("will add money to the balance when you deposit", function() {
+    var testAccount = new Account("Jon Snow", 2000);
+    expect(testAccount.deposit(500)).to.equal(2500);
+  });
+
+  it("will take away money to the balance when you withdraw", function() {
+    var testAccount = new Account("Jon Snow", 2000);
+    expect(testAccount.withdraw(500)).to.equal(1500);
+  });
 });
